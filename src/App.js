707a003aa0d5a09f07370ef706a034aceda98a0d
Julie Tran <kimtran767@gmail.com>
2021-09-21 11:52:17 -0700

    added monster name and cleaned up data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asr1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asr3'
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>
+            { monster.name}
+          </h1>)
+        }
+      </div>
+    )
+  }
+
 }
 
 export default App;

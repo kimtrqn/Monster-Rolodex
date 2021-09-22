332452a636fab7442d6bec653002efbd7ca20b72
Julie Tran <kimtran767@gmail.com>
2021-09-21 20:31:19 -0700

    added cardlist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import CardList from './components/card-list/cardList.jsx'
 import './App.css';
 
 class App extends React.Component {
@@ -7,30 +8,25 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asr1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asr3'
-        },
-      ]
+      monsters: [],
     }
+  }
+
+  componentDidMount() { //fetch a data which will return a promise
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className='app'>
         {
-          this.state.monsters.map(monster => <h1 key={monster.id}>
-            { monster.name}
-          </h1>)
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>
+              { monster.name}
+            </h1>
+          ))
         }
       </div>
     )
---
+++ src/components/card-list/cardList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const CardList = (props) => {
+
+    return (
+        <div> 
+        
+        {console.log(props)}
+            Hello World
+        </div>
+    )
+};
+
+
+export default CardList

0f86c51d404ed4c32d69fe67eb4a121a1efdb9a2
Julie Tran <kimtran767@gmail.com>
2021-09-22 09:40:57 -0700

    added monster image from site as img url

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './card.css';
+
 const Card = (props) => {
 
     return(
-        <div>
+        <div className='card-container'>
+            <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
             <h1 key={props.monster.id}>{props.monster.name}</h1>
         </div>
     )
